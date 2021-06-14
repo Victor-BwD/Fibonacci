d5ad39ef07990c20cb4f043ca805f94caa577719
Victor-BwD <victor.bogdanowdornelles@gmail.com>
2021-06-14 18:45:47 -0300

    Finish

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 1, n2 = 0, soma, tamDaSequencia;
+
+            tamDaSequencia = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < tamDaSequencia; i++)
+            {
+                soma = n1 + n2;
+
+                Console.WriteLine(soma);
+                n2 = n1;
+                n1 = soma;
+
+            }
+        }
+    }
+}
